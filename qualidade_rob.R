8d9c2b4cdbb1a6cc5c7881ad547409ae31fcca32
Tamires Martins <tamiresmartins.fln@gmail.com>
2022-02-28 11:41:56 -0300

    plot rob syrcle em percentual e primeiro rascunho do plot trafficlight

--- qualidade_rob.R
+++ qualidade_rob.R
@@ -9,7 +9,8 @@ library(tidyverse)    # manipulacao de dados e plotar
 library(patchwork)    # juntar plots
 library(extrafont)    # adicionar fontes
 library(cowplot)      # salvar plot
-
+library(robvis)
+library(readxl)
 
 # Carregar dataframe dos dados limpos e organizados
 
@@ -19,34 +20,51 @@ df <-
 
 # Isolar variáveis do ROB SYRCLE
 
-
 df_rob <- df %>% 
-  mutate(estudo = str_c(first_author, ", ", lubridate::year(year))) %>% 
-  select(starts_with("rob"), estudo)
+  mutate(Study = str_c(first_author, ", ", lubridate::year(year))) %>% 
+  select(starts_with("rob"), Study) 
 
 
 df_rob <- df_rob %>% 
-  pivot_longer(!estudo,
+  distinct() # deixar uma linha por publicação
+
+df_rob <- df_rob %>% 
+  rename("A alocação de tratamento foi adequadamente gerada e aplicada?" = rob1,
+         "Os grupos (controle e tratado) eram similares no início do experimento?" = rob2,
+         "A alocação foi adequadamente escondida?" = rob3,
+         "Os animais foram acondicionados aleatoriamente?" = rob4, 
+         "Os investigadores eram cegos quanto ao tratamento durante os experimentos?" = rob5, 
+         "Os animais foram selecionados aleatoriamente para acessar o desfecho?" = rob6,
+         "A avaliação do resultado foi cega?" = rob7, 
+         "Dados incompletos foram adequadamente endereçados?" = rob8, 
+         "Os relatos do estudo são livres de seleção de desfecho relatado?" = rob9,
+         "O estudo está aparentemente livre de algum outro problema que poderia resultar em alto risco de viés?" = rob10) %>% 
+         mutate(Weight = as.numeric(1),
+                overall = "Unclear") %>% 
+  relocate(Study, everything())
+
+
+
+
+df_rob_long <- df_rob %>% # colocar em modo longo
+  pivot_longer(!c(Study, Weight),
                names_to = "pergunta",
                values_to = "atribuicao",
-               )
+               ) 
 
-df_rob$pergunta <- fct_relevel(df_rob$pergunta, "rob1", "rob2", "rob3", "rob4", "rob5", "rob6", "rob7", "rob8", "rob9", "rob10")
 
 # Renomear os níveis 
 
-df_rob$atribuicao <-
+df_rob_long$atribuicao <-
   factor(
-    df_rob$atribuicao,
+    df_rob_long$atribuicao,
     levels = c("Yes", "No", "Unclear"),
-    labels = c("Baixo risco", "Alto risco", "Incerto") # mudar
+    labels = c("Baixo risco", "Alto risco", "Incerto") # colocar significado das atribuições
   )
 
-df_rob$pergunta <-
-  factor(
-    df_rob$pergunta,
-    levels = c("rob1", "rob2", "rob3", "rob4", "rob5", "rob6", "rob7", "rob8", "rob9", "rob10"),
-    labels = c("A alocação de tratamento foi adequadamente gerada e aplicada?",
+df_rob_long$pergunta <-
+  fct_relevel(
+    df_rob_long$pergunta, "A alocação de tratamento foi adequadamente gerada e aplicada?",
                "Os grupos (controle e tratado) eram similares no início do experimento?", 
                "A alocação foi adequadamente escondida?", 
                "Os animais foram acondicionados aleatoriamente?", 
@@ -55,8 +73,7 @@ df_rob$pergunta <-
                "A avaliação do resultado foi cega?",
                "Dados incompletos foram adequadamente endereçados?", 
                "Os relatos do estudo são livres de seleção de desfecho relatado?", 
-               "O estudo está aparentemente livre de algum outro problema que poderia resultar em alto risco de viés?")
-  )
+               "O estudo está aparentemente livre de algum outro problema que poderia resultar em alto risco de viés?") # renomear para a pergunta completa) # estabeler ordem para as perguntas
 
 
 # Estabelecer tema base para todos os próximos gráficos
@@ -65,13 +82,13 @@ theme_set(theme_minimal(base_family = "Gadugi"))
 
 
 
-# Visualização ROB SYRCLE
+# Visualização ROB SYRCLE Resumo
 
 v_factor_levels <- c("Alto risco", "Incerto", "Baixo risco")
 
-robplot <- df_rob %>% 
-  group_by(estudo) %>% 
-  distinct(estudo, pergunta, atribuicao) %>% 
+robplot <- df_rob_long %>% 
+  group_by(Study) %>% 
+  distinct(Study, pergunta, atribuicao) %>% 
   ggplot(aes(x = fct_rev(fct_infreq(pergunta)), fill = factor(atribuicao, levels = v_factor_levels), y = ..count..)) +
   geom_bar(position = "fill") + 
   scale_fill_manual("Julgamento do risco de viés", values = c("Baixo risco" = "#82c236", "Incerto" = "#fec200", "Alto risco" = "#ec2b2b")) +
@@ -106,5 +123,26 @@ save_plot(filename = "Figura18.png",
           plot = robplot,
           dpi = 300)
 
+# Visualização ROB SYRCLE Traffic light
 
 
+rob <- read_excel("data/rob.xlsx")
+
+
+
+robplot_farol <- rob_traffic_light(data = rob[1:10,], tool = "ROB1", colour = c("#82c236", "#fec200", "#ec2b2b"), psize = 5)
+
+
+robplot_farol <- robplot_farol + geom_point(size = 5)
+
+
+
+
+save_plot(filename = "Figura19.png",
+          plot = robplot_farol,
+          dpi = 300)
+
+robplot_farol
+
+
+rob_summary(df_rob, tool = "ROB1", overall = FALSE, weighted = FALSE)
